fd73b000d9c3045eba98cea0b2e1769d289cdd13
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-09-09 11:27:03 -0400

    Initial Commit

---
+++ C++_TotalTemplateSpecialization/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++_TotalTemplateSpecialization
+//
+//  Created by Ferenc Szalma on 9/9/16.
+//  Copyright © 2016 Ferenc Szalma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
