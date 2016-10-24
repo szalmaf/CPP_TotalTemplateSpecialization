01e9487a499bdfd25989ee979f9647e05d071826
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-10-24 12:26:32 -0400

    Add MsgSender template class hierarchy; CompanyA, B, and a specialized Z

--- C++_TotalTemplateSpecialization/main.cpp
+++ C++_TotalTemplateSpecialization/main.cpp
@@ -7,9 +7,73 @@
 //
 
 #include <iostream>
+#include <string>
+
+class CompanyA {
+public:
+    void sendClearText(const std::string & msg) {};
+    void sendEncrypted(const std::string & msg) {};
+};
+
+class CompanyB {
+public:
+    void sendClearText(const std::string & msg) {};
+    void sendEncrypted(const std::string & msg) {};
+};
+
+class CompanyZ {
+public:
+    void sendEncrypted(const std::string & msg) {};
+};
+
+class MsgInfo {
+
+};
+
+template<typename Company>
+class MsgSender { // Base class
+public:
+    void sendClear(const MsgInfo & info) {
+        std::string msg;
+        Company c;
+        c.sendClearText(msg);
+    }
+    void sendSecret(const MsgInfo & info) {
+        std::string msg;
+        Company c;
+        c.sendEncrypted(msg);
+    }
+};
+
+template<>
+class MsgSender<CompanyZ> { // total template specialization of MsgSender to CompanyZ
+public:
+    void sendSecret(const MsgInfo & info) {
+    }
+};
+
+template<typename Company>
+class LoggingMsgSender : public MsgSender<Company> {
+public:
+//    using MsgSender<Company>::sendClear; // brings sendClear into scope for sendClear(info)
+    void sendClearMsg(const MsgInfo & info) {
+//        sendClear(info); // won't compile w/o using above
+        this->sendClear(info); // will compile even w/o using
+        MsgSender<Company>::sendClear(info); // will also compile
+    }
+};
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    // MsgSender usage example
+    MsgInfo mi;
+    MsgSender<CompanyB> mscb;
+    mscb.sendClear(mi);
+    
+    // LoggingMsgSender usage example
+    LoggingMsgSender<CompanyA> lms;
+    lms.sendClearMsg(MsgInfo());
     return 0;
 }
